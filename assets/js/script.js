a622e3d94f0fe9c4df0af6f6c4345fc507c2c3cc
Marta Paz <pazmarta@gmail.com>
2019-05-18 11:26:36 +0000

    create table where numbers will be inserted

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,16 +7,41 @@ document.querySelector('#start').addEventListener('click', function(){
   });
 
   this.remove();
-})
+});
 
 // make function that create the game
 function memo(conf){
-
-    // Numbers that going to play
-    numbers = [];
+    
+    var d = document, // document
+        t = d.createElement('table'), // table
+        g = { // game data
+            started: false,
+            gameOver: false
+            // time = 0;
+        };
+        
+    // numbers that going to play
+    var numbers = [];
     for(var i = 1; i <= (conf.columns*conf.rows) / 2; i++){
         numbers.push(i);
         console.log(numbers);
+    }
+    
+    // create table
+    for(var i = 0; i < conf.rows; i++){
+        var tr = d.createElement('tr');
+
+        for(var j = 0; j < conf.columns; j++){
+            var td = d.createElement('td'),
+                span = d.createElement('span');
+
+            // append to table
+            tr.appendChild(td);
+            td.appendChild(span);
+        }
+
+        t.appendChild(tr);
+        console.log(t);
     }
 
 }
