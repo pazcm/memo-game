12f6939568ef930f77bf6ff782ac15ea4156f6e7
Marta Paz <pazmarta@gmail.com>
2019-05-18 18:46:01 +0000

    add start game function

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,12 +26,12 @@ function memo(conf){
     var nums = [];
     for(var i = 1; i <= (conf.columns*conf.rows) / 2; i++){
         nums.push(i);
-        console.log(nums);
+        // console.log(nums);
     }
     
     // duplicate numbers
     var numbers = nums.concat(nums);
-    console.log(numbers);
+    // console.log(numbers);
     
     // shuffle numbers
     numbers.sort(function() { return 0.5 - Math.random() });
@@ -55,13 +55,41 @@ function memo(conf){
         }
 
         t.appendChild(tr);
-        console.log(t);
+        // console.log(t);
     }
     
     // create board where numbers will be displayed and append table to it
     t.className = 'game-board';
     b.appendChild(t);
     console.log(b);
+    
+    // append panel controls to the board
+    var controls = d.createElement('div');
+    controls.className = 'game-controls';
+    b.appendChild(controls);
+    console.log(controls);
+    
+    // append comments to the board
+    var comments = d.createElement('div');
+    comments.className = 'game-comments';
+    b.appendChild(comments);
+
+    // start game function + timer after 5 seconds ..
+    function start(){
+        var time = Math.ceil((conf.columns * conf.rows)*0.25);
+
+        var interval = setInterval(function(){
+            controls.innerHTML = 'The game starts in ... ' + time + ' seconds';
+
+            if(time === 0){
+                g.started = true;
+
+                clearInterval(interval);
+            } else time--;
+        }, 1000);
+    }
+    start();
+    
     
     
 }
